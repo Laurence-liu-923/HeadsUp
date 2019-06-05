51ae6666ae1ae6ff8dc8cadbdb087e45b2371b21
yliu123 <yang.7.liu@nokia.com>
2019-06-05 17:07:07 +0200

    Basic layout with a single charade

--- HeadsUp/HeadsUpViewController.swift
+++ HeadsUp/HeadsUpViewController.swift
@@ -10,10 +10,21 @@ import UIKit
 
 class HeadsUpViewController: UIViewController {
 
+    @IBOutlet weak var currentCharade: UILabel!
+//    let charadeList = ["Daenarys Targarian", "Jon Snow", "Sansa Stark"]
+    
+    let charadeList = [1,2,3,4,5,6,7,8,9,10]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        currentCharade.layer.borderWidth = 10.0
+        currentCharade.layer.cornerRadius = 16
+        currentCharade.layer.borderColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
     }
 
+    
+    
 }
 
