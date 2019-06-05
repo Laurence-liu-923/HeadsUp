c45e0ea80dd5da9a0b701f4c7971c6f3dc4ff9ed
yliu123 <yang.7.liu@nokia.com>
2019-06-05 13:48:08 +0200

    Initial Commit

---
+++ HeadsUp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HeadsUp
+//
+//  Created by Liu, Yang 7. (Nokia - FR/Paris-Saclay) on 05/06/2019.
+//  Copyright © 2019 LaurenceLiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
